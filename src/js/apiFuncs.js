430e4db2138d82f9092a9aca1de682062955deba
N0tAdm1n <n0tadm1n29@gmail.com>
2022-12-04 21:29:06 +0530

    Add getUserLocation()

--- src/js/apiFuncs.js
+++ src/js/apiFuncs.js
@@ -1,11 +1,21 @@
+import * as domFuncs from "./domFuncs";
+
 // function to get current weather of a city
-async function getCurrentWeatherData(cityName) {
+async function getCurrentWeatherData() {
+  const coordinates = await getUserLocation();
+  const lat = coordinates.coords.latitude;
+  const lon = coordinates.coords.longitude;
+
+  // console.log(coordinates);
+  // console.log(coordinates.coords);
+  // console.log(coordinates.coords.longitude);
+
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=7e8e0d0ed87de6d0cad1b65469377040`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=7e8e0d0ed87de6d0cad1b65469377040&units=metric`
   );
   const data = await response.json();
 
-  return data;
+  console.log(data);
 }
 
 // function to get weather forercast data for 5 days in 3hr intervals
@@ -22,11 +32,24 @@ async function getForecastData(cityName = "Kanpur") {
 }
 
 // function to get the city geographical data(coordinates)
-async function getCoordinates(cityName) {
+async function getCoordinates(cityName = "Kanpur") {
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=7e8e0d0ed87de6d0cad1b65469377040`
   );
-  return await response.json();
+  const coordinates = await response.json();
+  console.log(coordinates);
 }
 
-export { getCurrentWeatherData, getForecastData };
+// get user device location using geolocation api
+function getUserLocation() {
+  return new Promise((res, rej) => {
+    navigator.geolocation.getCurrentPosition(res, rej);
+  });
+}
+
+export {
+  getCurrentWeatherData,
+  getForecastData,
+  getCoordinates,
+  getUserLocation,
+};
