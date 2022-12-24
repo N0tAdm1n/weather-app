993046f96d4bbfc5c6f63dfb94b1ca50deb2df53
N0tAdm1n <n0tadm1n29@gmail.com>
2022-12-24 20:47:13 +0530

    Use utilFuncs

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,17 @@
 import * as apiFuncs from "./apiFuncs";
 import * as domFuncs from "./domFuncs";
+import * as utilFuncs from "./utilFuncs";
 import "../style.css";
 
 const renderPage = async () => {
-  const data = await apiFuncs.getCurrentWeatherData();
-  console.log(Math.round(data.main.feels_like));
-  domFuncs.updateMainWeather(data);
+  const currentWeatherData = await apiFuncs.getCurrentWeatherData();
+  domFuncs.updateMainWeather(currentWeatherData);
+
+  const forecastWeatherData = await apiFuncs.getForecastData();
+  const datedForecastWeatherArray = utilFuncs.sortIntoDatedArray(
+    forecastWeatherData.list
+  );
+  domFuncs.updateUpcomingWeatherWraper(datedForecastWeatherArray);
 };
 
 renderPage();
